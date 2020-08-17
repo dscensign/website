31c24a6e4d9fbd8a6b823df867b20bfb4763f43f
Ricardo Prins <ricardo@iamprins.com>
2020-08-16 23:00:04 -0600

    Initial deployment

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2aTemplates
-from fastapi.staticfiles import StaticFiles
+"""
+Python file for webpage deployment for DSC Ensign College
 
 """
-Vou documentar essa bagaça direito!
-"""
+
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI(
+    title='DSC Ensign College',
+    description='DSC Ensign College Website',
+    version='1.0'
+)
+app.mount('/assets', StaticFiles(directory='assets'), name='static')
+templates = Jinja2Templates(directory='assets')
+
+
+@app.get('/')
+def index(request: Request):
+    """Main page"""
+    return templates.TemplateResponse('index.html', {
+        'request': request
+    })
+
+@app.get('/join')
+def join(request: Request):
+    """Join page"""
+    return templates.TemplateResponse('join.html', {
+        'request': request
+    })
